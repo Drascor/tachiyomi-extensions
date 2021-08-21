596427912b114ea4396876eada1531946e0d0ba1
Drascor <admin@drascor.com>
2021-08-21 11:28:12 +0200

    Aumentar ratelimit

--- src/es/tumangaonline/src/eu/kanade/tachiyomi/extension/es/tumangaonline/TuMangaOnline.kt
+++ src/es/tumangaonline/src/eu/kanade/tachiyomi/extension/es/tumangaonline/TuMangaOnline.kt
@@ -558,7 +558,7 @@ class TuMangaOnline : ConfigurableSource, ParsedHttpSource() {
         private const val IMAGE_CDN_RATELIMIT_PREF_SUMMARY = "Este valor afecta la cantidad de solicitudes de red para descargar imágenes. Reducir este valor puede disminuir errores al cargar imagenes, pero la velocidad de descarga será más lenta. Se requiere reiniciar Tachiyomi. \nValor actual: %s"
         private const val IMAGE_CDN_RATELIMIT_PREF_DEFAULT_VALUE = "10"
 
-        private val ENTRIES_ARRAY = (1..10).map { i -> i.toString() }.toTypedArray()
+        private val ENTRIES_ARRAY = (1..32).map { i -> i.toString() }.toTypedArray()
 
         const val PREFIX_LIBRARY = "library"
         const val PREFIX_ID_SEARCH = "id:"
