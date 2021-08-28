16dcf2fd710f730df6674fc77ca63773eb558890
Drascor <admin@drascor.com>
2021-08-29 00:34:05 +0200

    Change rate limit

--- src/es/lectormanga/src/eu/kanade/tachiyomi/extension/es/lectormanga/LectorManga.kt
+++ src/es/lectormanga/src/eu/kanade/tachiyomi/extension/es/lectormanga/LectorManga.kt
@@ -537,7 +537,7 @@ class LectorManga : ConfigurableSource, ParsedHttpSource() {
         private const val IMAGE_CDN_RATELIMIT_PREF_SUMMARY = "Este valor afecta la cantidad de solicitudes de red para descargar imágenes. Reducir este valor puede disminuir errores al cargar imagenes, pero la velocidad de descarga será más lenta. Se requiere reiniciar Tachiyomi. \nValor actual: %s"
         private const val IMAGE_CDN_RATELIMIT_PREF_DEFAULT_VALUE = "10"
 
-        private val ENTRIES_ARRAY = (1..10).map { i -> i.toString() }.toTypedArray()
+        private val ENTRIES_ARRAY = (1..30).map { i -> i.toString() }.toTypedArray()
 
         const val PREFIX_ID_SEARCH = "id:"
         const val MANGA_URL_CHUNK = "gotobook"
